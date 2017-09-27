412ab870a76dbedaac8045e69d33fffe25619852
David Waring <dev@davidwaring.net>
2017-09-27 16:17:12 -0400

    Format & get changes
    code format changes
    ensure get only selects 1 row

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const sqlite3 = require("sqlite3").verbose();
+const sqlite3 = require('sqlite3');
 
 
 /**
@@ -16,51 +16,77 @@ const sqlite3 = require("sqlite3").verbose();
  */
 class RightTrackDB {
 
-    /**
-     * Right Track Database Constructor
-     * @constructor
-     * @param {string} id Agency ID
-     * @param {string} location File path to the Right Track database
-     */
-    constructor(id, location) {
-        this.id = id;
-        this.location = location;
-        this.db = new sqlite3.Database(location);
-    }
+  /**
+   * Right Track Database Constructor
+   * @constructor
+   * @param {string} id Agency ID
+   * @param {string} location File path to the Right Track database
+   */
+  constructor(id, location) {
+    this.id = id;
+    this.location = location;
+    this.db = new sqlite3.Database(location);
+  }
 
-    /**
-     * Select multiple rows from the database
-     * @param {string} statement Select Statement
-     * @param {RightTrackDB~selectCallback} callback Select callback function
-     */
-    select(statement, callback) {
-        this.db.all(statement, function(err, rows) {
-            if (err) {
-                console.error("ERROR SELECTING RESULTS FROM DB");
-                console.error(statement);
-                console.error(err);
-                return callback(err);
-            }
-            return callback(null, rows);
-        });
-    }
+  /**
+   * Select multiple rows from the database
+   * @param {string} statement Select Statement
+   * @param {RightTrackDB~selectCallback} callback Select callback function
+   */
+  select(statement, callback) {
+    this.db.all(statement, function(err, rows) {
 
-    /**
-     * Select a single row from the database
-     * @param {string} statement Select Statement
-     * @param {RightTrackDB~getCallback} callback Get callback function
-     */
-    get(statement, callback) {
-        this.db.get(statement, function(err, row) {
-            if (err) {
-                console.error("ERROR SELECTING FIRST RESULT FROM DB");
-                console.error(statement);
-                console.error(err);
-                return callback(err);
-            }
-            return callback(null, row);
-        });
-    }
+      // SQLite Error
+      if (err) {
+        console.error('ERROR SELECTING RESULTS FROM DB');
+        console.error(statement);
+        console.error(err);
+        return callback(err);
+      }
+
+      // Return Results
+      return callback(null, rows);
+
+    });
+  }
+
+  /**
+   * Select a single row from the database.  This ensures that only a single
+   * row is selected.  It will return an error if 0 or more than 1 rows
+   * are returned.
+   * @param {string} statement Select Statement
+   * @param {RightTrackDB~getCallback} callback Get callback function
+   */
+  get(statement, callback) {
+    this.db.all(statement, function(err, rows) {
+
+      // SQLite ERROR
+      if (err) {
+        console.error('ERROR SELECTING FIRST RESULT FROM DB');
+        console.error(statement);
+        console.error(err);
+        return callback(err);
+      }
+
+      // No Result
+      if ( rows.length === 0 ) {
+        return callback(
+          new Error('No Result returned from select/get statement')
+        );
+      }
+
+      // Too many results
+      else if ( rows.length > 1 ) {
+        return callback(
+          new Error('More than 1 result returned from select/get statement')
+        );
+      }
+
+      // Return Result
+      return callback(null, rows[0]);
+
+    });
+  }
 
 }
 
